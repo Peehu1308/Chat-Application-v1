a799f612319eebc59c51646ae5cb776befad8c6e
Peehu Mishra <peehumishra1308@gmail.com>
2026-04-29 22:57:53 +0530

    Update OtherUsers.jsx

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import OtherUser from './OtherUser'
+import useGetOtherUsers from '../hooks/useGetOtherUsers'
 
 const OtherUsers = () => {
+    useGetOtherUsers();
   return (
-    <div>
-        
-            <div className='flex gap-2 items-center hover:bg-zinc-200 rounded-sm p-2 cursor-pointer'>
-                <div className='avatar avatar-online'>
-                    <div>
-                        <img src="https://wallpapers.com/images/hd/cool-profile-picture-87h46gcobjl5e4xu.jpg" alt="user profile"  className='h-10 w-10 border border-white rounded-full bg-white'/>
-                    </div>
-                </div>
-                <div className='flex flex-col'>
-                    <div className='flex justify-between gap-2 flex-1'>
-                        <p>Peehu</p>
-                    </div>
-                </div>
-            </div>
-        
+    <div className='overflow-y-scroll h-[400px] '>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
+        <OtherUser/>
     </div>
   )
 }
