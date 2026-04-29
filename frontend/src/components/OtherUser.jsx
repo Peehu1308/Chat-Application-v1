2d6e0afac4a64b88b3c89a14d4a6253d084729b4
Peehu Mishra <peehumishra1308@gmail.com>
2026-04-29 22:58:09 +0530

    Create OtherUser.jsx

---
+++ frontend/src/components/OtherUser.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const OtherUser = () => {
+  return (
+    <div>
+        
+            <div className='flex gap-2 items-center hover:bg-zinc-200 rounded-sm p-2 cursor-pointer'>
+                <div className='avatar avatar-online'>
+                    <div>
+                        <img src="https://wallpapers.com/images/hd/cool-profile-picture-87h46gcobjl5e4xu.jpg" alt="user profile"  className='h-10 w-10 border border-white rounded-full bg-white'/>
+                    </div>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='flex justify-between gap-2 flex-1'>
+                        <p>Peehu</p>
+                    </div>
+                </div>
+            </div>
+            <div className='divider my-0 py-0'>
+
+            </div>
+        
+    </div>
+  )
+}
+
+export default OtherUser
