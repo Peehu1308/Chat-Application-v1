7e49cd576008468eac7164a4b0714f1ef08ae5ca
Peehu Mishra <peehumishra1308@gmail.com>
2026-04-30 09:34:52 +0530

    Update OtherUser.jsx

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const OtherUser = () => {
+const OtherUser = (props) => {
+    const user=props.user;
   return (
     <div>
         
             <div className='flex gap-2 items-center hover:bg-zinc-200 rounded-sm p-2 cursor-pointer'>
                 <div className='avatar avatar-online'>
                     <div>
-                        <img src="https://wallpapers.com/images/hd/cool-profile-picture-87h46gcobjl5e4xu.jpg" alt="user profile"  className='h-10 w-10 border border-white rounded-full bg-white'/>
+                        <img src={user?.profilePhoto} alt="user profile"  className='h-10 w-10 border border-white rounded-full bg-white'/>
                     </div>
                 </div>
                 <div className='flex flex-col'>
                     <div className='flex justify-between gap-2 flex-1'>
-                        <p>Peehu</p>
+                        <p>{user?.fullName}</p>
                     </div>
                 </div>
             </div>
