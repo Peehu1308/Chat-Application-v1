9219a8e32b915b234caf191babcafcc773c2b712
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-02 12:21:02 +0530

    Create useGetMessages.jsx

---
+++ frontend/src/hooks/useGetMessages.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMessages } from "../redux/messageSlice";
+
+const useGetMessages = () => {
+    const {selectedUser}=useSelector(store=>store.user);
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const fetchMessages=async()=>{
+            if(!selectedUser?._id) return;
+            try{
+                axios.defaults.withCredentials=true;
+                const res=await axios.get(`http://localhost:8000/api/v1/message/${selectedUser._id}`);
+                console.log("Messages fetched:", res.data);
+                dispatch(setMessages(res.data));
+            }
+            catch(err){
+                console.log("Error fetching messages:", err);
+            }
+        }
+        fetchMessages();
+    },[selectedUser])
+}
+
+export default useGetMessages
