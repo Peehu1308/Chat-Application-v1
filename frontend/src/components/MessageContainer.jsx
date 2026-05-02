862dee4a3ca5cd753ab1d1be927874ab29194195
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-02 18:26:14 +0530

    Update MessageContainer.jsx

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SendInput from './SendInput'
 import Messages from './Messages'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { setSelectedUser } from '../redux/userslice'
 
 const MessageContainer = () => {
-    const {selectedUser}=useSelector(store=>store.user)
+    const {selectedUser}=useSelector(store=>store.user);
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        return ()=>dispatch(setSelectedUser(null));
+    },[])
   return (
     <div className='md:min-w-[450px] flex flex-col min-h-[400px] overflow-scroll"'>
             
