aeee8d20598bdc60befff9ad0c2f5e3587200e43
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-03 11:57:24 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,9 +12,24 @@ const io=new Server(server,{
 
     },
 });
+const userSocketMap={}; //userid->socketid
+
 
 io.on('connection',(socket)=>{
     console.log("user connected to socket io",socket.id);
+
+    const userId=socket.handshake.query.userId;
+    if(userId!==undefined){
+        userSocketMap[userId]=socket.id;
+    }
+    io.emit('getOnlineUser',Object.keys(userSocketMap));
+
+    socket.on('disconnect',()=>{
+        console.log('userdisconnected',socket.id);
+        delete userSocketMap[userId];
+        io.emit('getOnlineUser',Object.keys(userSocketMap));
+    })
+
 })
 
 export { app, io, server };
